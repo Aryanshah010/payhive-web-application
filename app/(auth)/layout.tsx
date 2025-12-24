dffe9bc95bc20e4ffbdc6372699059f41912ca30
Aryan shah <shaharyan844@gmail.com>
2025-12-24 20:30:35 +0545

    designed layout for authentication

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,20 +2,33 @@ import Image from "next/image";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <section className="h-screen">
-      <div className="h-full w-full grid md:grid-cols-2 md:gap-0">
-        <div className="relative hidden md:block max-h-4/5 overflow-hidden">
-          <Image
-            src="/auth-illustration.png"
-            alt="Welcome illustration"
-            fill
-            priority
-            className="object-cover"
-          />
+    <section className="min-h-screen">
+      <div className="grid min-h-screen md:grid-cols-2">
+
+        <div className="hidden md:flex flex-col">
+          <header className="py-4 px-2 md:py-6 lg:py-10 text-center shrink-0 ">
+            <h2 className="text-2xl sm:text-3xl lg:text-4xl font-semibold tracking-tight text-foreground">
+              Welcome to <span className="text-primary">PayHive</span>
+            </h2>
+
+            <p className="mt-2 sm:mt-3 text-sm  sm:text-base leading-relaxed  text-muted-foreground">
+              Your smart and secure digital wallet for everyday payments.
+            </p>
+          </header>
+
+          <div className="relative flex-1  overflow-hidden">
+            <Image
+              src="/auth-illustration.png"
+              alt="Welcome illustration"
+              fill
+              className="object-contain"
+            />
+          </div>
         </div>
 
-        <div className="flex h-full items-center justify-center px-4 md:px-10">
-          <div className="w-full max-w-md  border bg-background/80 supports-backdrop-filter:backdrop-blur p-6 shadow-sm">
+
+        <div className="flex items-center justify-center px-4 md:px-10">
+          <div className="w-full max-w-md border border-black/10 dark:border-white/10 bg-background/80 supports-backdrop-filter:backdrop-blur p-6 shadow-sm">
             {children}
           </div>
         </div>
