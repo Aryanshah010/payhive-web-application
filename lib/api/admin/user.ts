2f84a7d23db7eccf1cd115a848372662ecb05d8c
Aryan shah <shaharyan844@gmail.com>
2026-02-01 21:57:12 +0545

    Created CRUD api for admin

--- lib/api/admin/user.ts
+++ lib/api/admin/user.ts
@@ -21,22 +21,54 @@ export const createUser = async (userData: any) => {
     }
 }
 
-//update one user
+export const updateOneUser = async (id: string, userData: any) => {
+    try {
+        const endpoint = API.ADMIN.USER.UPDATE.replace(':id', id);
+        const response = await axios.put(
+            endpoint,
+            userData,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                }
+            }
+        );
+        return response.data;
+    } catch (error: Error | any) {
+        throw new Error(error.response?.data?.message
+            || error.message || 'Update user failed');
+    }
+}
 
 
+export const getAllUsers = async () => {
+    try {
+        const response = await axios.get(API.ADMIN.USER.READALL);
+        return response.data;
+    } catch (error: Error | any) {
+        throw new Error(error.response?.data?.message
+            || error.message || 'Get all users failed');
+    }
+}
 
+export const getOneUser = async (id: string) => {
+    try {
+        const endpoint = API.ADMIN.USER.READONE.replace(':id', id);
+        const response = await axios.get(endpoint);
+        return response.data;
+    } catch (error: Error | any) {
+        throw new Error(error.response?.data?.message
+            || error.message || 'Get user failed');
+    }
+}
 
-//getAllUsers
-
-
-
-
-
-
-
-//get/view one user
-
-
-
-
-//delete one user
+export const deleteOneUser = async (id: string) => {
+    try {
+        const endpoint = API.ADMIN.USER.DELETE.replace(':id', id);
+        const response = await axios.delete(endpoint);
+        return response.data;
+    } catch (error: Error | any) {
+        throw new Error(error.response?.data?.message
+            || error.message || 'Delete user failed');
+    }
+}
